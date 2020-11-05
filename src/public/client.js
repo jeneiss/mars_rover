2e0ef59e5dafbc12cc1ed8f7e0d6c1265a122543
montresor34 <8246292+montresor34@users.noreply.github.com>
2020-11-05 10:14:24 +0000

    Add nodemon for hot reloading; semi-colons for style

--- src/public/client.js
+++ src/public/client.js
@@ -29,7 +29,7 @@ const App = (state) => {
                 <h3>Put things on the page!</h3>
                 <p>Here is an example section.</p>
                 <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
+                    In fact, this website is one of
                     the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
                     This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
                     applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,16 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const path = require('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 // your API calls
 
@@ -19,10 +19,10 @@ app.get('/apod', async (req, res) => {
     try {
         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
-        res.send({ image })
+        res.send({ image });
     } catch (err) {
         console.log('error:', err);
     }
-})
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
